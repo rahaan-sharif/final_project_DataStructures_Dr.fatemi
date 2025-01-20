7d55451292381c9d64b49b1242af3115609089cb
amir hosein moradi <rahaansharif01@gmail.com>
2025-01-21 00:43:16 +0330

    created filling some functions of the classes

--- main.cpp
+++ main.cpp
@@ -63,16 +63,92 @@ class users
 private:
     string username;
     int password;
-    linked_list<string> posts;
-    graph<users> followings;
-    graph<users> followers;
+
+    linked_list<string>* posts;
+    graph<users>* followings;
+    graph<users>* followers;
+
+    users* next, *prev;
 public:
-    users()
-    {}
-    void sign_up()
-    {}
-    void delete_account()
-    {}
+    users(string username_in, int password_in)
+    {
+        password=password_in;
+        username=username_in;
+        posts=NULL;
+        followings=NULL;
+        followers=NULL;
+        next=NULL;
+        prev=NULL;
+    }
+    void sign_up(users*& start)
+    {
+        string username_in;
+        int password_in;
+        cout<<"enter new username: ";
+        cin>>username_in;
+        cout<<"enter new passwod: ";
+        cin>>password_in;
+
+        if(start==NULL)
+        {
+            start=new users(username_in, password_in);
+            cout<<"account created succesfully.\n";
+            return;
+        }
+        else
+        {
+            users* tmp_users=start;
+            while(tmp_users->next!=NULL)
+            {
+                if(tmp_users->username==username_in)
+                {
+                    cout<<"error! invalid username.\n";
+                    return;
+                }
+                tmp_users=tmp_users->next;
+            }
+            if(tmp_users->username==username_in)
+            {
+                cout<<"error! invalid username.\n";
+                return;
+            }
+            tmp_users->next=new users(username_in, password_in);
+            tmp_users->next->prev=tmp_users;
+            return;
+        }
+    }
+    void delete_account(users*& start_users)
+    {
+        string username_in;
+        int password_in;
+        cout<<"enter user's username to be removed: ";
+        cin>>username_in;
+        cout<<"enter password: ";
+        cin>>password_in;
+
+        users* tmp_users=start_users;
+        while(tmp_users!=NULL)
+        {
+            if(tmp_users->username==username_in && tmp_users->password==password_in)
+            {
+                tmp_users->prev->next=tmp_users->next;
+                tmp_users->next->prev=tmp_users->prev;
+                users* tmp_users2=start_users;
+                while(tmp_users2!=NULL)
+                {
+                    tmp_users2->unfollow(username_in);
+                    tmp_users2->remove_follower(username_in);
+                    tmp_users2=tmp_users2->next;
+                }
+
+                delete tmp_users;
+                return;
+            }
+            tmp_users=tmp_users->next;
+        }
+        cout<<"invalid username\n";
+        return;
+    }
     void watch_profile()
     {}
     void give_relative_accounts()
@@ -82,6 +158,12 @@ public:
     void add_new_post()
     {}
     void delete_post()
+    {}
+    void follow()
+    {}
+    void unfollow()
+    {}
+    void remove_follower()
     {}
 
     string get_data_from_file(string location)
@@ -111,4 +193,3 @@ int main(void)
 
     return 0;
 }
-//upload
