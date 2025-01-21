d989b15f019028a0120cd68dd3efa727c7259f76
amir hosein moradi <rahaansharif01@gmail.com>
2025-01-21 03:49:29 +0330

    created filling some functions of the classes

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 template <typename T>
-class linked_list 
+class linked_list
 {
 private:
     T data;
@@ -19,159 +19,418 @@ public:
         next = NULL;
         prev = NULL;
     }
-    void add_node(linked_list *&start_ll, T data_in) 
+    void add_node(linked_list *&start_ll, T data_in)
     {
-        if(start_ll==NULL)
+        if (start_ll == NULL)
         {
-            start_ll=new linked_list(0, data_in);
+            start_ll = new linked_list(0, data_in);
             return;
         }
         else
         {
-            linked_list* tmp_ll=start_ll;
-            while(tmp_ll->next!=NULL)
+            linked_list *tmp_ll = start_ll;
+            while (tmp_ll->next != NULL)
             {
-                tmp_ll=tmp_ll->next;
+                tmp_ll = tmp_ll->next;
             }
-            tmp_ll->next=new linked_list(tmp_ll->id+1, data_in);
-            tmp_ll->next->prev=tmp_ll;
+            tmp_ll->next = new linked_list(tmp_ll->id + 1, data_in);
+            tmp_ll->next->prev = tmp_ll;
             return;
         }
     }
 };
 
-
 template <typename T>
-class graph   //this ADT is for storing relations of users.
+class graph // this ADT is for storing relations of users.
 {
 private:
-    linked_list<T>* sart_ll;      
+    linked_list<T> *sart_ll;
+
 public:
     graph()
-    {}
-    void follow()
-    {}
-    void unfollow()
-    {}
+    {
+    }
+    void add_node()
+    {
+    }
 };
-
-
-
 
 class users
 {
 private:
+    static int total_users;
+
     string username;
     int password;
 
-    linked_list<string>* posts;
-    graph<users>* followings;
-    graph<users>* followers;
+    linked_list<string> *posts;
+    graph<users> *followings;
+    graph<users> *followers;
 
-    users* next, *prev;
+    users *next, *prev;
+
 public:
     users(string username_in, int password_in)
     {
-        password=password_in;
-        username=username_in;
-        posts=NULL;
-        followings=NULL;
-        followers=NULL;
-        next=NULL;
-        prev=NULL;
+        password = password_in;
+        username = username_in;
+        posts = NULL;
+        followings = NULL;
+        followers = NULL;
+        next = NULL;
+        prev = NULL;
+        total_users++;
     }
-    void sign_up(users*& start)
+    void sign_up(users *&start)
     {
         string username_in;
         int password_in;
-        cout<<"enter new username: ";
-        cin>>username_in;
-        cout<<"enter new passwod: ";
-        cin>>password_in;
+        cout << "enter new username: ";
+        cin >> username_in;
+        cout << "enter new passwod: ";
+        cin >> password_in;
 
-        if(start==NULL)
+        if (start == NULL)
         {
-            start=new users(username_in, password_in);
-            cout<<"account created succesfully.\n";
+            start = new users(username_in, password_in);
+            cout << "account created succesfully.\n";
             return;
         }
         else
         {
-            users* tmp_users=start;
-            while(tmp_users->next!=NULL)
+            users *tmp_users = start;
+            while (tmp_users->next != NULL)
             {
-                if(tmp_users->username==username_in)
+                if (tmp_users->username == username_in)
                 {
-                    cout<<"error! invalid username.\n";
+                    cout << "error! invalid username.\n";
                     return;
                 }
-                tmp_users=tmp_users->next;
+                tmp_users = tmp_users->next;
             }
-            if(tmp_users->username==username_in)
+            if (tmp_users->username == username_in)
             {
-                cout<<"error! invalid username.\n";
+                cout << "error! invalid username.\n";
                 return;
             }
-            tmp_users->next=new users(username_in, password_in);
-            tmp_users->next->prev=tmp_users;
+            tmp_users->next = new users(username_in, password_in);
+            tmp_users->next->prev = tmp_users;
             return;
         }
     }
-    void delete_account(users*& start_users)
+    void main_operations(users *&start)
+    {
+            cout << "wellcome to your social network!\n";
+            cout << "number of total users: " << total_users << endl<<endl;
+        int operation = -1;
+        while(operation != 0)
+        {        
+            cout << "choose your operation: " << endl;
+            cout << "1- sign up\n";
+            cout << "2- sign in\n";
+            cout<<"0- exit\n";
+            cin >> operation;
+            if (operation == 0)
+            {
+                cout<<"goodbye!\n";
+                return;
+            }
+            else if (operation == 1)
+            {
+                start->sign_up(start);
+            }
+            else if (operation == 2)
+            {
+                break;
+            }
+            else
+            {
+                cout<<"error! invalid input.\n";
+            }
+        }
+
+        string username_in;
+        int password_in;
+        cout << "** log in **" << endl;
+
+        cout << "enter username: ";
+        cin >> username_in;
+        cout << "enter password: ";
+        cin >> password_in;
+
+        users *cur_user = start;
+        while (cur_user != NULL)
+        {
+            if (cur_user->username == username_in && cur_user->password == password_in)
+            {
+                cout << "\t loged in...\n";
+                break;
+            }
+            cur_user = cur_user->next;
+        }
+        if (cur_user == NULL)
+        {
+            cout << "error! invalid username or password.\n";
+            return;
+        }
+        while (operation != 0)
+        {
+            cout << "welcome " << username << endl;
+            cout << "choose your operation:\n";
+            cout << "1- change username or password\n";
+            cout<<  "2- delete_account\n";
+            cout<<  "3- watch profile\n";
+            cout<<  "4- suggest friend\n";
+            cout<<  "5- search user\n";
+            cout<<  "6- add new post\n";
+            cout<<  "7- edit posts\n";
+            cout<<  "8- delete post\n";
+            cout<<  "9- unfollow\n";
+            cout<<  "10-remove follower\n";
+            cout<<  "0- exit\n";
+
+            cin>>operation;
+            if(operation==1)
+                {cur_user->change_username_or_password(cur_user);}
+            else if(operation==2)
+            {
+                cur_user->delete_account(start, cur_user);
+                return;
+            }
+            else if(operation==3)
+                {cur_user->watch_profile(cur_user);}
+            else if(operation==4)
+                {}
+            else if(operation==5)
+                {start->search_user(start);}
+            else if(operation==6)
+                {cur_user->add_new_post(cur_user);}
+            else if(operation==7)
+                {cur_user->edit_posts(cur_user);}
+            else if(operation==8)
+                {cur_user->delete_post(cur_user);}
+            else if(operation==9)
+                {cur_user->unfollow(cur_user);}
+            else if(operation==10)
+                {cur_user->remove_follower(cur_user);}
+            else if(operation==0)
+            {
+                cout<<"goodby!\n";
+                return;
+            }
+            else
+            {
+                cout<<"error! invalid operation.\n";
+            }
+        }
+    }
+    void change_username_or_password(users*& start)
     {
         string username_in;
         int password_in;
-        cout<<"enter user's username to be removed: ";
-        cin>>username_in;
-        cout<<"enter password: ";
-        cin>>password_in;
-
-        users* tmp_users=start_users;
-        while(tmp_users!=NULL)
+        cout<<"choose operation: "<<endl;
+        cout<<"1- change password\n";
+        cout<<"2- change username\n";
+        cout<<"3- change both\n";
+        int operation=-1;
+        cin>>operation;
+        if(operation==1)
         {
-            if(tmp_users->username==username_in && tmp_users->password==password_in)
-            {
-                tmp_users->prev->next=tmp_users->next;
-                tmp_users->next->prev=tmp_users->prev;
-                users* tmp_users2=start_users;
-                while(tmp_users2!=NULL)
-                {
-                    tmp_users2->unfollow(username_in);
-                    tmp_users2->remove_follower(username_in);
-                    tmp_users2=tmp_users2->next;
-                }
+            cout<<"enter password: ";
+            cin>>password_in;
+            start->password=password_in;
+            cout<<"changed\n";
+        }
+        else if(operation==2)
+        {
+            cout<<"enter username: ";
+            cin>>username_in;
+            start->username=username_in;
+            cout<<"changed\n";
+        }
+        else if(operation==3)
+        {
+            cout<<"enter username: ";
+            cin>>username_in;
+            start->username=username_in;
+            cout<<"enter password: ";
+            cin>>password_in;
+            start->password=password_in;
+            cout<<"changed\n";
+        }
+        else
+        {
+            cout<<"error! invalid operation\n";
+            return;
+        }
+    }
+    void delete_account(users*& start_user, users *&cur_user)
+    {
+        if(cur_user->prev==NULL)
+        {
+            start_user=cur_user->next;
+        }
+        else if(cur_user->next==NULL)
+        {
+            cur_user->prev->next=NULL;
+        }
+        else
+        {
+            cur_user->prev->next=cur_user->next;
+            cur_user->next->prev=cur_user->prev;
+        }
+        users* tmp_user=start_user;
+        while(tmp_user!=NULL)
+        {
+            tmp_user->unfollow(tmp_user, cur_user->username);
+            tmp_user->remove_follower(tmp_user, cur_user->username);
+            tmp_user=tmp_user->next;
+        }
 
-                delete tmp_users;
+        delete cur_user;
+        total_users--;
+    }
+    void watch_profile(users *&cur_user)// yours or your friend
+    {
+        int operation=-1;
+        cout<<"choose operation: ";
+        cout<<"1- watch your profile\n";
+        cout<<"2- watch your following profile\n";
+        cin>>operation;
+        if(operation==1)
+        {
+            cout<<"username: "<<cur_user->username<<endl;
+            cout<<"your posts: \n";
+            cur_user->posts->show(cur_user->posts);
+            cout<<"your followers:\n";
+            cur_user->followers->show(cur_user->followers);
+            cout<<"your followings: \n";
+            cur_user->followings->show(cur_user->followings);
+            return;
+        }
+        else if(operation==2)
+        {
+            cout<<"your followings:\n";
+            cur_user->followings->show(cur_user->followings);
+            cout<<"choose any username to watch profile: \n";
+            string username_in;
+            cin>>username_in;
+            users* tmp_user=cur_user->followings->get_node(cur_user->followings, username_in);
+            cout<<"username: "<<tmp_user<<endl;
+            cout<<"post: "<<endl;
+            tmp_user->posts->show(tmp_user->posts);
+            cout<<"followers: \n";
+            tmp_user->followers->show(tmp_user->followers);
+            cout<<"followings: \n";
+            tmp_user->followings->show(tmp_user->followings);
+        }
+        else
+        {
+            cout<<"error! invalid operation\n";
+            return;
+        }
+
+    }
+    void suggest_others_users(users*& start, users*& cur_user)
+    {
+        users* tmp_user=start;
+        users** users_list=new users*[6];
+        while(tmp_user!=NULL)
+        {
+            if(tmp_user!=cur_user)
+            {
+                if(!cur_user->followings->search(cur_user->followings, tmp_user))
+                {
+                    if(!cur_user->followers->search(cur_user->followers, tmp_user))
+                    {
+                        
+                    }
+                }
+            }
+            tmp_user=tmp_user->next;
+        }
+    }
+    void search_user(users*& start, users*& cur_user)
+    {
+        string username_in;
+        cout<<"enter username: ";
+        cin>>username_in;
+        users* tmp_user=start;
+        while(tmp_user!=NULL)
+        {
+            if(tmp_user->username==username_in)
+            {
+                cout<<"found\n";
+                cout<<"do you want to follow? 0-no 1-yes\n";
+                int check=-1;
+                cin>>check;
+                if(check)
+                    cur_user->follow(cur_user, tmp_user);
                 return;
             }
-            tmp_users=tmp_users->next;
+            tmp_user=tmp_user->next;
         }
-        cout<<"invalid username\n";
+        cout<<"not found\n";
         return;
     }
-    void watch_profile()
-    {}
-    void give_relative_accounts()
-    {}
-    void search_user()
-    {}
-    void add_new_post()
-    {}
-    void delete_post()
-    {}
-    void follow()
-    {}
-    void unfollow()
-    {}
-    void remove_follower()
-    {}
+    void add_new_post(users*& start)
+    {   
+        cout<<"enter your post: ";
+        string new_post;
+        getline(cin, new_post);
+        start->posts->add_node(start->posts, new_post);
+        cout<<"uploading post is finished\n";
+    }
+    void edit_posts(users*& start)
+    {
+        cout<<"your posts: ";
+        start->posts->show(start->posts);
+        cout<<"choose your post id to edit: ";
+        int id_in;
+        cin>>id_in;
+        cout<<"enter new version of the post: ";
+        string post_in;
+        getline(cin, post_in);
+        start->posts->edit_node(start->posts, id_in, post_in);
+    }
+    void delete_post(users*& start)
+    {
+        cout<<"your posts: ";
+        start->posts->show(start->posts);
+        cout<<"choose your post id to be deleted: ";
+        int id_in;
+        cin>>id_in;
+        start->posts->delete_node(start->posts, id_in);
+    }
+    void follow(users*& cur_user, users*& tmp_user)
+    {
+        cur_user->followings->add_node(cur_user->followings, tmp_user);
+    }
+    void unfollow(users*& cur_user)
+    {   
+        cout<<"followings: "<<endl;
+        cur_user->followings->show(cur_user->followings);
+        cout<<"enter account username to be deleted from your followings: ";
+        string username_in;
+        getline(cin, username_in);
+        cur_user->followings->delete_node(cur_user->followings, username_in);
+    }
+    void remove_follower(users*& cur_user)
+    {   
+        cout<<"followers: "<<endl;
+        cur_user->followers->show(cur_user->followers);
+        cout<<"enter account username to be deleted from your followings: ";
+        string username_in;
+        getline(cin, username_in);
+        cur_user->followers->delete_node(cur_user->followers, username_in);
+    }
 
     string get_data_from_file(string location)
     {
         ifstream input_file(location);
-        if(!input_file)
+        if (!input_file)
         {
-            cout<<"error! invalid location for text file.\n";
+            cout << "error! invalid location for text file.\n";
             return NULL;
         }
         else
@@ -183,10 +442,7 @@ public:
         }
     }
 };
-
-
-
-
+int users::total_users = 0;
 
 int main(void)
 {
